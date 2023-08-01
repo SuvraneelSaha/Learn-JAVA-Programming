1df04e71e072f5a06ea186fc80ae7715ba42e032
Suvraneel _Saha <suvroneel2001@gmail.com>
2023-08-01 09:34:09 +0530

    Welcome program

---
+++ Welcome.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+
+public class Welcome
+{
+	public static void main(String nigga[])
+	{
+		Scanner in = new Scanner(System.in);
+		String name;
+		System.out.println("May I know your Name");
+		name=in.nextLine();
+		
+		System.out.println("Welcome "+name);
+	}
+}
